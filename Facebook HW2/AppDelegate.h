aa07b97c8880040eddc601cabc5ab41a4539262e
Sophie Tang <sophietang@measurefrom-lm.corp.yahoo.com>
2014-06-06 17:17:56 -0700

    Initial Commit

---
+++ Facebook HW2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Facebook HW2
+//
+//  Created by Sophie Tang on 6/6/14.
+//  Copyright (c) 2014 Sophie Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Facebook HW2Tests/Facebook_HW2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Facebook_HW2Tests.m
+//  Facebook HW2Tests
+//
+//  Created by Sophie Tang on 6/6/14.
+//  Copyright (c) 2014 Sophie Tang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Facebook_HW2Tests : XCTestCase
+
+@end
+
+@implementation Facebook_HW2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
